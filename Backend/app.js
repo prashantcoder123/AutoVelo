5df393e62ded6de1364d49066ea121437da2dd4f
prashant kumar <prashantkum7676@gmail.com>
2026-04-08 17:42:29 +0530

    add endpoints

--- Backend/app.js
+++ Backend/app.js
@@ -5,17 +5,17 @@ const app = express();
 const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
-
-
+const cookieParser = require('cookie-parser');
 
 connectToDb();
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.get('/',(req,res) =>{
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
-app.use('/users',userRoutes);
+app.use('/users', userRoutes);
 
 
 
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-//const blackListTokenModel = require('../models/blackListToken.model');
+const blackListTokenModel = require('../models/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -63,18 +63,19 @@ module.exports.loginUser = async (req, res, next) => {
 
 
 
-// module.exports.getUserProfile = async (req, res, next) => {
+module.exports.getUserProfile = async (req, res, next) => {
 
-//     res.status(200).json(req.user);
+    res.status(200).json(req.user);
 
-// }
+}
 
-// module.exports.logoutUser = async (req, res, next) => {
-//     res.clearCookie('token');
-//     const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+module.exports.logoutUser = async (req, res, next) => {
 
-//     await blackListTokenModel.create({ token });
+    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
 
-//     res.status(200).json({ message: 'Logged out' });
+    await blackListTokenModel.create({ token });
 
-// }
+    res.clearCookie('token');
+
+    res.status(200).json({ message: 'Logged out' });
+}
